afa2ff4487e3b005f6b6f69839c8eb4c0a5b4cea
Radion Rusnak <radion.rusnak@gmail.com>
2026-02-22 16:54:15 +0100

    [main] simple demo page

--- example/frontend/src/App.tsx
+++ example/frontend/src/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
+    return (
+        <main className="min-h-screen px-6 py-10 grid place-items-center select-none">
+            <section className="w-full max-w-3xl text-center">
+                <h1 className="m-0 text-7xl font-bold">Zystal</h1>
+                <p className="mx-auto mt-4 max-w-[56ch] text-lg font-medium">
+                    Cross-platform self-contained web applications in Zig
+                </p>
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+                <button
+                    type="button"
+                    className="mt-7 rounded-2xl bg-[#FAF9F5] hover:bg-[#BFBFBF] text-[#141413] px-4 py-2 text-md font-semibold cursor-pointer active:scale-[0.98] transition-transform duration-150"
+                    onClick={() => setCount(count + 1)}
+                >
+                    Count: {count}
+                </button>
+            </section>
+        </main>
+    );
 }
 
-export default App
+export default App;
